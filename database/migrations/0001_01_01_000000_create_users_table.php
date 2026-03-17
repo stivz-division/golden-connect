b55a5420ed8fc0a8a4e75b9bb4b99affb854972b
division <stivz-division@mail.ru>
2026-03-17 19:35:51 +0700

    feat(auth): добавить Laravel Fortify и расширить структуру users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('login')->unique()->index();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('surname');
+            $table->string('email')->index()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
