d5acd25dc5bac47f68172085f9fff43a82370b39
division <stivz-division@mail.ru>
2026-03-19 14:30:19 +0700

    fix: ci

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL);
         $middleware->web(append: [
             SetLocale::class,
             HandleInertiaRequests::class,
